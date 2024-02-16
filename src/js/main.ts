352930d65e260e5ddc08bc7721a0a10b72df9cc1
Kasumi Teshima <info@cumak.net>
2024-02-16 14:38:28 +0900

    feat: developersページ作成

--- src/js/main.ts
+++ src/js/main.ts
@@ -2,12 +2,15 @@
 import jump from 'jump.js';
 import { easeOutExpo } from 'ez.js';
 import Splide from '@splidejs/splide';
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
 import { gsap } from 'gsap/dist/gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 import { CSSRulePlugin } from 'gsap/CSSRulePlugin';
 gsap.registerPlugin(CSSRulePlugin);
 import { ArrowPositionExtension } from './ArrowPositionExtension';
+
+let toolkitFilterType = 'ul';
 
 class MyFunc {
   /**
@@ -40,15 +43,34 @@ class MyFunc {
     return val;
   }
 
-  slider() {
+  getMediaQuery() {
+    const widthXL = Number(
+      getComputedStyle(document.body)
+        .getPropertyValue('--widthXL')
+        .replace(/[^0-9]/g, '')
+    );
+    const widthLG = Number(
+      getComputedStyle(document.body)
+        .getPropertyValue('--widthLG')
+        .replace(/[^0-9]/g, '')
+    );
     const widthTab =
       Number(
         getComputedStyle(document.body)
           .getPropertyValue('--widthTab')
           .replace(/[^0-9]/g, '')
       ) - 1;
+    return { widthXL, widthLG, widthTab };
+  }
 
-    const splide = new Splide('.splide', {
+  slider() {
+    const splideEl = document.getElementById('slider1');
+    if (!splideEl) {
+      return false;
+    }
+    const widthTab = this.getMediaQuery().widthTab;
+
+    const splide = new Splide('#slider1', {
       type: 'loop',
       focus: 0,
       autoWidth: true,
@@ -307,7 +329,7 @@ class MyFunc {
             }
           });
           //ボタンのテキストを「開く」に戻す
-          const btn = accordionItem.querySelector('.js-accordion-title');
+          const btn = accordionItem.querySelector('.js-accordion-more');
           btn.textContent = btn.getAttribute('data-open');
         } else {
           //開く
@@ -325,7 +347,7 @@ class MyFunc {
             }
           });
           //ボタンのテキストを「閉じる」に変更
-          const btn = accordionItem.querySelector('.js-accordion-title');
+          const btn = accordionItem.querySelector('.js-accordion-more');
           btn.textContent = btn.getAttribute('data-close');
         }
       }
@@ -341,6 +363,181 @@ class MyFunc {
       }
     });
   }
+
+  tabChange() {
+    const tabWrap = document.querySelector<HTMLElement>('.js-tab-wrap');
+    if (!tabWrap) {
+      return false;
+    }
+    const tabContentsWrap = tabWrap.querySelector<HTMLElement>('.js-tabContents-wrap');
+    const tabs = tabWrap.querySelectorAll('.js-tabone');
+    const tabContents = tabWrap.querySelectorAll<HTMLElement>('.js-tabContents');
+
+    const contentHeights = [...tabContents].map((tabContent) => tabContent.clientHeight);
+    const contentStyle = getComputedStyle(tabContentsWrap);
+
+    clickTab_lsr(tabWrap.querySelector<HTMLElement>('.js-tabone.is-current'));
+
+    tabs.forEach((tab) => {
+      tab.addEventListener('click', function (e) {
+        tabs.forEach((t) => t.classList.remove('is-current'));
+        const target = e.target as HTMLElement;
+        clickTab_lsr(target);
+      });
+    });
+
+    function clickTab_lsr(target: HTMLElement) {
+      if (!tabWrap) {
+        return false;
+      }
+
+      //targetのid属性を取得する
+      const tabId = target.getAttribute('id');
+      target.classList.add('is-current');
+
+      tabContents.forEach((tabContent) => {
+        //tabContentのクラスにtabIDを含んでいたら
+        if (tabContent.classList.contains(tabId)) {
+          tabContent.classList.add('is-current');
+        } else {
+          tabContent.classList.remove('is-current');
+        }
+      });
+
+      //高さを変える
+      const index = Array.from(tabs).indexOf(target);
+      const padding =
+        Number(contentStyle.paddingTop.replace('px', '')) + Number(contentStyle.paddingBottom.replace('px', ''));
+      tabContentsWrap.style.height = `${contentHeights[index] + padding}px`;
+    }
+  }
+
+  sliderAuto() {
+    const splideEl = document.getElementById('slider2');
+    if (!splideEl) {
+      return false;
+    }
+    const splide = new Splide('#slider2', {
+      type: 'loop',
+      focus: 'center',
+      pagination: false,
+      arrows: false,
+      cloneStatus: false,
+      autoWidth: true,
+      drag: false,
+      autoScroll: {
+        pauseOnHover: false,
+        speed: 2,
+      },
+    });
+
+    splide.mount({
+      AutoScroll,
+    });
+  }
+
+  changeToolFilterType() {
+    let isChange = false;
+    let type = 'ul';
+    const widthLG = this.getMediaQuery().widthLG;
+    if (widthLG > window.innerWidth) {
+      type = 'select';
+    }
+    if (toolkitFilterType !== type) {
+      isChange = true;
+      type = toolkitFilterType;
+    }
+    return { type, isChange };
+  }
+
+  // ulをselectに変換
+  convertSelect() {
+    const widthLG = this.getMediaQuery().widthLG;
+    const jsConvert = document.querySelector('.js-convert');
+    if (!jsConvert) {
+      return false;
+    }
+    const jsConvertUl = jsConvert?.querySelector('.js-convert-ul');
+    if (widthLG > window.innerWidth) {
+      const selectEl = document.querySelector('.js-converted-select');
+      if (selectEl) {
+        return false;
+      }
+      const optionItemsEl = jsConvert?.querySelectorAll<HTMLElement>('.js-convert-option');
+      const optionItemsTextArr = Array.from(optionItemsEl).map((el) => el.textContent);
+      const dataIndexes = Array.from(optionItemsEl).map((el) => el.closest('button').dataset.index);
+      const newSelectEl = document.createElement('select');
+      newSelectEl.classList.add('js-converted-select');
+      newSelectEl.setAttribute('aria-hidden', 'true');
+      newSelectEl.setAttribute('tabindex', '-1');
+      newSelectEl.setAttribute('aria-label', '選択してください');
+      newSelectEl.innerHTML = optionItemsTextArr
+        .map((text, i) => {
+          return `<option value="${dataIndexes[i]}">${text}</option>`;
+        })
+        .join('');
+      jsConvertUl.classList.add('is-hidden');
+      jsConvert?.appendChild(newSelectEl);
+    } else {
+      const selectEl = jsConvert?.querySelector('.js-converted-select');
+      if (!selectEl) {
+        return false;
+      }
+      selectEl.remove();
+      jsConvertUl?.classList.remove('is-hidden');
+    }
+  }
+
+  toolkitFilter() {
+    const toolkitFilter = document.querySelector<HTMLElement>('.js-toolkit-filter');
+    if (!toolkitFilter) {
+      return false;
+    }
+
+    const li = toolkitFilter.querySelectorAll('li');
+    const select = toolkitFilter.querySelector<HTMLSelectElement>('.js-converted-select');
+
+    if (!select) {
+      li.forEach((el) => {
+        const btn = el.querySelectorAll('button');
+
+        btn.forEach((btnEl) => {
+          const classStr = btnEl.classList;
+          if (classStr.contains('is-active')) {
+            const defaultIndex = btnEl.dataset.index;
+            filtering(defaultIndex);
+          }
+          btnEl.addEventListener('click', function () {
+            const index = this.dataset.index;
+            filtering(index);
+            li.forEach((el) => {
+              const childBtn = el.querySelector('button');
+              childBtn.classList.remove('is-active');
+            });
+            btnEl.classList.add('is-active');
+          });
+        });
+      });
+    } else {
+      filtering('1');
+      select.addEventListener('change', function () {
+        const index = this.value;
+        filtering(index);
+      });
+    }
+
+    function filtering(index: string) {
+      const panels = document.querySelector('.js-filter-panels');
+      const panelItems = panels?.querySelectorAll<HTMLElement>('.js-filter-panel');
+      panelItems.forEach((el) => {
+        el.classList.remove('is-active');
+        const arr = el.dataset.filter?.split(',');
+        if (arr?.includes(index)) {
+          el.classList.add('is-active');
+        }
+      });
+    }
+  }
 }
 
 const myFunc = new MyFunc();
@@ -354,10 +551,16 @@ window.addEventListener('load', function () {
   myFunc.slider();
   myFunc.numberShowAnimation();
   myFunc.accordion();
+  myFunc.tabChange();
+  myFunc.sliderAuto();
+  myFunc.convertSelect();
+  myFunc.toolkitFilter();
 });
 
-// window.addEventListener('resize', function () {
-// });
+window.addEventListener('resize', function () {
+  myFunc.tabChange();
+  myFunc.changeToolFilterType();
+});
 window.addEventListener('scroll', function () {
   myFunc.scrollBodyClass();
 });
